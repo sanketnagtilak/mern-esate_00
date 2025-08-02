532e72039a40b69c388abee0f99bee246731bb7d
sanketnagtilak <sanketnagtilak1@gmail.com>
2025-08-02 20:15:11 +0530

    v 0.0.4

--- api/index.js
+++ api/index.js
@@ -1,11 +1,11 @@
 import express from 'express';
 import mongoose from 'mongoose';
 
-mongoose.
-
+mongoose.connect("mongodb+srv://sanke:sanket@mern-estate.mtbhqxe.mongodb.net/mern-esate?retryWrites=true&w=majority&appName=mern-estate")
 
 const app = express();
 
 app.listen(3000, () => {
-  console.log('Server is running on port 3000');
-});
+    console.log("Server is running on port 3000!");
+}
+)
