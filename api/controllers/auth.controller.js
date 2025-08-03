783be29427df01fda9da10e5b43bc2d6948876d8
sanketnagtilak <sanketnagtilak1@gmail.com>
2025-08-03 15:50:23 +0530

    v 0.0.8.5

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -9,7 +9,7 @@ export const signup = async (req, res) => {
         await newUser.save();
     res.status(201).json({ message: "User created successfully" });
     } catch (error) {
-        res.status(500).json({ error: error.message });
+        next(error);
     }
     
 };
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,31 +1,58 @@
+
 import { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate} from 'react-router-dom';
 
 export default function SignUp() {
-  const [formData, setFormData]=useState({})
+  const [formData, setFormData] = useState({})
+
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+
   const HashChangeEvent = (e) => {
     setFormData({
-        ...formData,
-        [e.target.id]: e.target.value
-      });
+      ...formData,
+      [e.target.id]: e.target.value
+    });
   };
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const res = await fetch ('/api/auth/signup',
-    {
-      method: 'POST',
-      headers:{
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(formData),
+    try {
 
-    }) ;
-    const data = await res.json();
-    console.log(data);
+      //loading state
+      setLoading(true);
+
+      const res = await fetch('/api/auth/signup',
+        {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(formData),
+
+        });
+      const data = await res.json();
+      console.log(data);
+      if (data.success === false) {
+        setLoading(false);
+        setError(data.message);
+
+        return;
+      }
+      setLoading(false);
+      setError(null);
+      navigate('/sign-in');
+    }
+    catch (error) {
+      setLoading(false);
+      setError("Something went wrong!");
+      console.error(error);
+    }
   };
+
   return (
     <div className='p-3 max-w-lg mx-auto'>
-    
+
       <h1 className='text-3xl text-center font-semibold my-7'>Sign Up</h1>
       <form onSubmit={handleSubmit} className='flex flex-col gap-4 '>
         <input
@@ -49,14 +76,17 @@ export default function SignUp() {
           id="password"
           onChange={HashChangeEvent}
         />
-        <button className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95' >Sign-Up</button>
+        <button disabled={loading} className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-80' >
+          {loading ? "Loading..." : "Sign Up"}
+        </button>
       </form>
-      <div className='flex gap-2 mt-5'> 
+      <div className='flex gap-2 mt-5'>
         <p>Have an account?</p>
         <Link to={"/SignIn"}>
           <span className='text-blue-500'>Sign in</span>
         </Link>
       </div>
+      {error && <p className='text-red-500 mt-3'>{error}</p>}
     </div>
   );
 }
