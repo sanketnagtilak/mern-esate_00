99ade864e1d7fd680ab2a76d8dc718cc14778619
sanketnagtilak <sanketnagtilak1@gmail.com>
2025-08-05 14:44:16 +0530

    v 0.1.1 (login page)

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -82,7 +82,7 @@ export default function SignUp() {
       </form>
       <div className='flex gap-2 mt-5'>
         <p>Have an account?</p>
-        <Link to={"/SignIn"}>
+        <Link to={"/sign-in"}>
           <span className='text-blue-500'>Sign in</span>
         </Link>
       </div>
